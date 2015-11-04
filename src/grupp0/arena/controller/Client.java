6df595f89f3387c1cf73fc545d9d15c0c8345749
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-11-04 11:03:58 +0100

    Type commit message here…

--- src/grupp0/arena/controller/Client.java
+++ src/grupp0/arena/controller/Client.java
@@ -1,7 +1,16 @@
 package grupp0.arena.controller;
 
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
 public class Client {
     public void run(String[] args) {
         System.out.println("HEYLOOOOO");
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(new Button("ASS was here!!!!11"));
+        stage.setScene(scene);
+        stage.showAndWait();
     }
 }

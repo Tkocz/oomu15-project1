e96ea072244a1098ff324ff860b30e966e6ffddd
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-11-04 13:59:40 +0100

    Type commit message here…

---
+++ src/grupp0/arena/model/User.java
@@ -0,0 +1,86 @@
+package grupp0.arena.model;
+
+/*------------------------------------------------
+ * IMPORTS
+ *----------------------------------------------*/
+
+import grupp0.arena.view.LoginWindow;
+
+/*------------------------------------------------
+ * CLASS
+ *----------------------------------------------*/
+
+/**
+ * Represents a single user.
+ *
+ * @author Philip Arvidsson (S133686)
+ */
+public abstract class User {
+
+/*------------------------------------------------
+ * FIELDS
+ *----------------------------------------------*/
+
+private final String name;
+private final String password;
+private final String realName;
+
+/*------------------------------------------------
+ * PUBLIC METHODS
+ *----------------------------------------------*/
+
+/**
+ * Gets the username.
+ *
+ * @return The username.
+ */
+public String getName() {
+    return (name);
+}
+
+/**
+ * Sets the username.
+ *
+ * @param value The username.
+ */
+public void setName(String value) {
+    name = value;
+}
+
+/**
+ * Gets the password.
+ *
+ * @return The password.
+ */
+public String getPassword() {
+    return (password);
+}
+
+/**
+ * Sets the password.
+ *
+ * @param value The password.
+ */
+public void setPassword(String value) {
+    password = value;
+}
+
+/**
+ * Gets the user's real name.
+ *
+ * @return The user's real name.
+ */
+public String getRealName() {
+    return (realName);
+}
+
+/**
+ * Sets the user's real name.
+ *
+ * @param value The user's real name.
+ */
+public void setRealName(String value) {
+    realName = value;
+}
+
+}

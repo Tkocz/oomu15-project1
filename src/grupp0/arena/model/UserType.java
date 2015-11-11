b7b34b092f2f17b412699479059a7a56b4112e03
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-11-11 12:32:04 +0100

    Type commit message here…

---
+++ src/grupp0/arena/model/UserType.java
@@ -0,0 +1,24 @@
+package grupp0.arena.model;
+
+/*------------------------------------------------
+ * CLASS
+ *----------------------------------------------*/
+
+/**
+ * Defines the user types.
+ *
+ * @author Philip Arvidsson (S133686)
+ */
+public enum UserType {
+
+/**
+ * Specifies that a user is a player.
+ */
+PLAYER,
+
+/**
+ * Specifies that a user is an operator.
+ */
+OPERATOR
+
+}

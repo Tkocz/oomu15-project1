2260cd5cfb37b0a85c4732ade6a36d8bb2841d01
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-11-11 15:15:37 +0100

    Type commit message here…

---
+++ src/grupp0/arena/model/Operator.java
@@ -0,0 +1,29 @@
+package grupp0.arena.model;
+
+/*------------------------------------------------
+ * IMPORTS
+ *----------------------------------------------*/
+
+import grupp0.arena.model.UserType;
+
+/*------------------------------------------------
+ * CLASS
+ *----------------------------------------------*/
+
+/**
+ * Represents an operator.
+ *
+ * @author Philip Arvidsson (S133686)
+ */
+public class Operator extends User {
+
+/*------------------------------------------------
+ * PUBLIC METHODS
+ *----------------------------------------------*/
+
+@Override
+public UserType getType() {
+    return (UserType.OPERATOR);
+}
+
+}

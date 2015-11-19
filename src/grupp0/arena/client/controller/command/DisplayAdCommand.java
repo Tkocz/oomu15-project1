c52bd1db4e7f4c5ec97efaa17e07ce4864ffae65
tobiasanhed <anhedtobias@gmail.com>
2015-11-19 13:27:24 +0100

    -

--- src/grupp0/arena/client/controller/command/DisplayAdCommand.java
+++ src/grupp0/arena/client/controller/command/DisplayAdCommand.java
@@ -6,16 +6,22 @@
 package grupp0.arena.client.controller.command;
 
 import grupp0.arena.Arena;
+import grupp0.arena.base.model.AdvertisementInfo;
+import grupp0.arena.client.controller.Client;
 
 /**
  *
  * @author Zzooki
  */
 public class DisplayAdCommand extends ClientNetworkCommand{
-    
+
     @Override
     public void perform(){
         //Visa ad i lobby
         Arena.trace("visar ad" + getArg(0));
+        String imageURL = getArg(0);
+        String linkURL  = getArg(1);
+        AdvertisementInfo ad = new AdvertisementInfo(imageURL, linkURL, null);
+        Client.getInstance().adProperty().setValue(ad);
     }
 }
